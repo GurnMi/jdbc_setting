92a9688cc0f7918fe61009f9d79c980d139df7c5
GurnMi <dlrjsak3@naver.com>
2017-10-10 15:04:48 +0900

    MySQL Connection Test Complete~~!

--- src/kr/or/dgit/jdbc_setting/jdbc/DBCon.java
+++ src/kr/or/dgit/jdbc_setting/jdbc/DBCon.java
@@ -3,6 +3,8 @@ package kr.or.dgit.jdbc_setting.jdbc;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBCon {
@@ -18,10 +20,17 @@ public class DBCon {
 	private DBCon(){
 		//내부에서 생성, 외부에서 생성 못하도록 생성자 만들어줌
 		Properties properties = getProperties("conf.properties");
-		System.out.println(properties.getProperty("user"));
+		try {
+			
+			connection = DriverManager.getConnection(properties.getProperty("url"),properties.getProperty("user"), properties.getProperty("pwd"));
+		} catch (SQLException e) {
+			System.out.printf("%s -%s%n",e.getErrorCode(), e.getMessage());
+			e.printStackTrace();
+		}
+		
+/*		System.out.println(properties.getProperty("user"));
 		System.out.println(properties.getProperty("pwd"));
-		System.out.println(properties.getProperty("url"));
-
+		System.out.println(properties.getProperty("url"));*/
 	}
 	
 	private Properties getProperties(String propertiesPath) {
