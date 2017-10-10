3d8fc5232bfc8f2ccd3da1d916568e22ce080013
GurnMi <dlrjsak3@naver.com>
2017-10-10 14:59:12 +0900

    setting 기본 완료 및 Properties 테스트

---
+++ src/kr/or/dgit/jdbc_setting/jdbc/DBCon.java
@@ -0,0 +1,44 @@
+package kr.or.dgit.jdbc_setting.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class DBCon {
+	
+	private static final DBCon instance = new DBCon();
+	private Connection connection;
+	
+	
+	public static DBCon getInstance() {
+		return instance;
+	}
+	
+	private DBCon(){
+		//내부에서 생성, 외부에서 생성 못하도록 생성자 만들어줌
+		Properties properties = getProperties("conf.properties");
+		System.out.println(properties.getProperty("user"));
+		System.out.println(properties.getProperty("pwd"));
+		System.out.println(properties.getProperty("url"));
+
+	}
+	
+	private Properties getProperties(String propertiesPath) {
+		Properties properties =  new Properties();
+		InputStream is = ClassLoader.getSystemResourceAsStream(propertiesPath);
+		try {
+			properties.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return properties;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	
+	
+}
